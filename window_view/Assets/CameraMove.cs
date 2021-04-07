8cb9478510e5886b71a5d2004b3612f372da7440
Suomeli <80705445+Suomeli@users.noreply.github.com>
2021-04-07 18:49:36 +0300

    Kameran siirto
    Kameran siirto haluttuihin koordinaatteihin käyttäen tyhjää targettia. Päivittyy joka kerta, kun ruutu päivittyy. Rotaatio vielä toimimaton.

---
+++ window_view/Assets/CameraMove.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+    public Vector3 offset;
+    public float turningRate = 30f;
+
+
+    void Update()
+    {
+        ///updates each frame to move camera towards desired position until reached
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+        ///updates rotation until desired rotation reached
+        Vector3 target_rotation = target.transform.localRotation.eulerAngles;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target_rotation, turningRate * Time.deltaTime);
+
+    }
+
+}
