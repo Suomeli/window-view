06dd3d21d9033caeb3b54ef63f43028e6c3f59a2
Suomeli <80705445+Suomeli@users.noreply.github.com>
2021-04-08 10:19:23 +0300

    Free Kameran if lause
    Vapaan kameran mahdollistavan if lauseen ja boolena-muuttujan lisääminen

--- window_view/Assets/CameraMove.cs
+++ window_view/Assets/CameraMove.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-
+    public bool FreeMode = false;
     public Transform target;
 
     public float smoothTime = 0.3f;
@@ -13,14 +13,24 @@ public class CameraMove : MonoBehaviour
 
     void Update()
     {
-        ///updates each frame to move camera towards desired position until reached
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
 
-        ///updates rotation until desired rotation reached
-        Quaternion target_rotation = Quaternion.Euler(target.transform.localRotation.eulerAngles);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, target_rotation, turningRate * Time.deltaTime);
+        if (FreeMode == false)
+        {
+            
+            ///updates each frame to move camera towards desired position until reached
+            Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
 
+            ///updates rotation until desired rotation reached
+            Quaternion target_rotation = Quaternion.Euler(target.transform.localRotation.eulerAngles);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, target_rotation, turningRate * Time.deltaTime);
+        }
+        else
+        {
+
+            Debug.Log("FreeMode");
+
+        }
     }
 
 }
