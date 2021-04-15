50416c74b41d00f358c47c9563ceca27ecd2642d
Suomeli <80705445+Suomeli@users.noreply.github.com>
2021-04-15 09:15:09 +0300

    Korkeuden laskentaparametrit
    Muuttujat korkeuden laskentaan maanpinnasta ja kerroksen mukaan. Kommentointia.

--- window_view/Assets/ui.cs
+++ window_view/Assets/ui.cs
@@ -14,11 +14,15 @@ public class ui : MonoBehaviour {
     public Vector3 target_coordinates = Vector3.zero;
     public Transform targetEmpty;
 
+    public float floor_height = 0f;
+    public float ground_height = 0f;
+
 
 
     // SendParameters is called when the user presses "OK"-button
     public void SendParameters()
     {
+        ///check for proper input
         try
         {
             ///Define possible decimal separators of number format
@@ -33,18 +37,25 @@ public class ui : MonoBehaviour {
             float direction = Convert.ToSingle(Convert.ToDouble(input_direction.text, formatinfo));
             Debug.Log("Annettu osoite: " + adress + " Annettu korkeus: " + height + " Annettu suunta: " + direction);
 
-            target_coordinates.Set(0f, height, 0f);
+            ///target height from floors and terrain
+
+            float target_height = height * floor_height + ground_height;
+
+            ///set target values to variables
+            target_coordinates.Set(0f, target_height, 0f);
             Quaternion target_rotation = Quaternion.Euler(0, direction, 0);
 
+            ///move target empty object to target values of transform and rotation
             targetEmpty.position = target_coordinates;
             targetEmpty.rotation = target_rotation;         
             
-
+            ///show in console
             Debug.Log(target_coordinates);
             Debug.Log(target_rotation);
 
 
         }
+        ///faulty input
         catch (Exception)
         {
             Debug.Log("Virheellinen syöte!");
