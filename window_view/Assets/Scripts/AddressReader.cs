4f462e6853438d4888008cee06bed5445e126ead
AnssiJussila <anssi.jussila@aalto.fi>
2021-04-29 12:49:11 +0300

    Päivitys geokoodaukseen
    Päivitetty versio geokoodaus skriptistä. Skriptin pitäisi pystyä löytämään kaikki tai ainakin suurimman osan osoitteista json tiedostosta.

--- window_view/Assets/Scripts/AddressReader.cs
+++ window_view/Assets/Scripts/AddressReader.cs
@@ -16,41 +16,52 @@ For example "/tiedostot/osoitteet_hki.json"
 Currently returns coordinates in int format. Float does not work as intended at the moment because
 of scientific notation. Unity shortens too large values. InputAddress string has to be exactly as
 the address in json file. There is no fuzzy search.
+
+sources:
+SimpleJSON by Bunny83
+Regular expression and code snippets translated to C# from HelsinkiGeocoder.py script by Henri Kotkanen and Jussi Arpalahti
 */
 public class AddressReader : MonoBehaviour
 {
     public List<int> returnCoordinates(string inputAddress, string jsonFileLocation) {
-                
         // Create an empty list for coordinates
         List<int> coordinates = new List<int>();
         
         // Trim whitespaces from front and end of the string. Replace multiple whitespace with one white space.
-        // Split the string and save the results to an array.
         inputAddress = inputAddress.Trim();
         inputAddress = Regex.Replace(inputAddress,@"\s+"," ");
-        string[] stringArray = inputAddress.Split(char.Parse(" "));
-        
-        // Store street name. Check if the selected indexes of array are null or not and save necessary data based on that.
-        string streetName = stringArray[0];
-        string buildingNumber = stringArray.ElementAtOrDefault(1) != null ? stringArray[1] : null;
-        string buildingLetter = stringArray.ElementAtOrDefault(2) != null ? stringArray[2] : null;
+
+        // Regular expression that is used to create groups of the input address
+        Regex regex = new Regex(@"^([^\d^/]*)(\s*[-\d]*)(\s*[A-ö]*)");
+        GroupCollection group = regex.Match(inputAddress).Groups;
+
+        // Storing group values at certain index to new variables
+        string streetName = group[1].Value.Trim();
+        string buildingNumber = group[2].Value.Any() ? group[2].Value.Trim() : null;
+        string buildingLetter = group[3].Value.Any() ? group[3].Value.Trim() : null;
+
+        // Combining building number and letter for later when comparing addresses
+        string buildingNumLet = buildingNumber != null ? buildingNumber : null;
+        buildingNumLet = buildingNumLet != null && buildingLetter != null ? buildingNumber+buildingLetter.ToLower() : buildingNumber;
 
         // Path of the json file. Read all lines into a string. Parse the json string via simpleJSON plugin
         string path = Application.dataPath + jsonFileLocation;
-
         string jsonString = File.ReadAllText(path);
         JSONNode data = JSON.Parse(jsonString);
 
         // Loop through features in json file
         foreach(JSONNode location in data["features"]) {
+
             // Check if the current street name matches json files street name
-            if (streetName == location["properties"]["katunimi"]) {
+            if (streetName.ToLower() == location["properties"]["katunimi"].Value.ToLower() || streetName.ToLower() == location["properties"]["gatan"].Value.ToLower()) {
                 // Check if the current osoitenumero_teksti is not null and matches the buildingNumber variable
-                if (buildingNumber == location["properties"]["osoitenumero_teksti"] && location["properties"]["osoitenumero_teksti"] != null) {
+                if (buildingNumLet == location["properties"]["osoitenumero_teksti"] && buildingNumLet != null) {
+                    // Debug.Log(location["properties"]["osoitenumero_teksti"]);
                     // Add coordinates to list
                     coordinates.Add(location["geometry"]["coordinates"][0]);
                     coordinates.Add(location["geometry"]["coordinates"][1]);
                 }
+
                 // Check if the osoitenumero_teksti is null
                 else if (location["properties"]["osoitenumero_teksti"] == null) {
                     // Add coordinates to list
@@ -59,7 +70,7 @@ public class AddressReader : MonoBehaviour
                 }
             }
         }
-        
+
         // Simple checker if list is empty or not
         if (coordinates.Count.Equals(0)) {
             Debug.Log("Did not find coordinates. Check address for typos.");
