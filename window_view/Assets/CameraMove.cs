1635691f92c2ffca8ba53b5e2c265d2a5fea0f35
Ngoc Chau <ngoc.chau@hotmail.com>
2021-04-29 12:15:47 +0300

    kameran-nuolinappaimet

--- window_view/Assets/CameraMove.cs
+++ window_view/Assets/CameraMove.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using TMPro;
 
 public class CameraMove : MonoBehaviour
 {
@@ -19,43 +18,18 @@ public class CameraMove : MonoBehaviour
     public float speedH = 2.0f;
     public float speedV = 2.0f;
 
-    private float yaw = -0.0f;
+    private float yaw = 0.0f;
     private float pitch = 0.0f;
-    public Vector3 rotation;
-
-
-    //variables for checkin if UI is active
-    public TMP_InputField field1;
-    public TMP_InputField field2;
-    public TMP_InputField field3;
-
-    private bool UI_active = false;
 
     void Update()
     {
-        //check if any inputfield is active
-        //this is quite a hackey way to do it, maybe better ways exist
-        //change UI_active variable based on results
-        if (field1.GetComponent<TMP_InputField>().isFocused || field2.GetComponent<TMP_InputField>().isFocused || field3.GetComponent<TMP_InputField>().isFocused)
-        {
-            UI_active = true;
-        }
-        else
-        {
-            UI_active = false;
-        }
-
-
         ///FreeMode toggle check
-        if (UI_active == false && Input.GetKeyDown("f"))
+        if (Input.GetKeyDown("f"))
         {
             FreeMode = !FreeMode;
         }
-        
-
         if (FreeMode == false)
         {
-            
             ///updates each frame to move camera towards desired position until reached
             Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, 0));
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
@@ -63,20 +37,45 @@ public class CameraMove : MonoBehaviour
             ///updates rotation until desired rotation reached
             Quaternion target_rotation = Quaternion.Euler(target.transform.localRotation.eulerAngles);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, target_rotation, turningRate * Time.deltaTime);
-            //get default view direction for freecam from target
-            yaw = target.transform.localRotation.eulerAngles.y;
 
+            ///updates the position of the target in X and Z axis by arrow keys
+            Vector3 new_targetPosition = ArrowKeyMovement();
+            new_targetPosition = new_targetPosition * Time.deltaTime * 5.0f;
+            target.Translate(new_targetPosition);
         }
-        else if(UI_active == false)
+        else
         {
             ///Freemode camera rotation
             yaw += speedH * Input.GetAxis("Mouse X");
             pitch -= speedV * Input.GetAxis("Mouse Y");
 
             transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-
-
         }
+
+    }
+
+    ///returns the values of the target position with made by keys
+    private Vector3 ArrowKeyMovement()
+    {
+        Vector3 positionArrows = new Vector3();
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            positionArrows += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            positionArrows += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            positionArrows += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            positionArrows += new Vector3(1, 0, 0);
+        }
+
+        return positionArrows;
     }
 
 }
